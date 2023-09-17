9d1266349ed455351f693de3f6c83740446271ab
ninjakidd96 <122418048+ninjakidd96@users.noreply.github.com>
2023-09-17 17:13:53 -0400

    Add files via upload
    The right way

--- calculator-test.js
+++ calculator-test.js
@@ -9,3 +9,61 @@ it("should return a result with 2 decimal places", function() {
 });
 
 /// etc
+describe("Loan Calculator", function() {
+  it('should calculate the correct monthly payment', function () {
+    // Arrange
+    const values = { amount: 10000, years: 5, rate: 5 };
+    
+    // Act
+    const monthlyPayment = calculateMonthlyPayment(values);
+
+    // Assert
+    expect(monthlyPayment).toEqual('188.71');
+  });
+
+  it('should handle zero principal', function () {
+    // Arrange
+    const values = { amount: 0, years: 5, rate: 5 };
+    
+    // Act
+    const monthlyPayment = calculateMonthlyPayment(values);
+
+    // Assert
+    expect(monthlyPayment).toEqual('0.00');
+  });
+
+  it('should handle zero interest rate', function () {
+    // Arrange
+    const values = { amount: 10000, years: 5, rate: 0 };
+    
+    // Act
+    const monthlyPayment = calculateMonthlyPayment(values);
+
+    // Assert
+    expect(monthlyPayment).toEqual('166.67');
+  });
+
+  it('should calculate the monthly rate correctly', function () {
+    // Arrange
+    const values = { amount: 10000, years: 5, rate: 6 };
+    
+    // Act
+    const monthlyPayment = calculateMonthlyPayment(values);
+
+    // Assert
+    expect(monthlyPayment).toEqual('193.33');
+  });
+
+  it("should return a result with 2 decimal places", function() {
+    // Arrange
+    const values = { amount: 20000, years: 3, rate: 4.5 };
+    
+    // Act
+    const monthlyPayment = calculateMonthlyPayment(values);
+
+    // Assert
+    expect(monthlyPayment).toMatch(/^\d+\.\d{2}$/); // Check if it has 2 decimal places
+  });
+
+  // Add more test cases as needed
+});
--- calculator.js
+++ calculator.js
@@ -38,3 +38,57 @@ function calculateMonthlyPayment(values) {
 // update the UI to show the value.
 function updateMonthly(monthly) {
 }
+// Add a DOMContentLoaded event listener to initialize the calculator
+window.addEventListener('DOMContentLoaded', function() {
+  const form = document.getElementById("calc-form");
+  if (form) {
+    setupInitialValues();
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+      update();
+    });
+  }
+});
+
+// Function to get the current input values from the UI
+function getCurrentUIValues() {
+  return {
+    amount: +(document.getElementById("loan-amount").value),
+    years: +(document.getElementById("loan-years").value),
+    rate: +(document.getElementById("loan-rate").value),
+  };
+}
+
+// Function to set default values in the inputs and calculate the initial monthly payment
+function setupInitialValues() {
+  // Set default values in the inputs (you can customize these)
+  document.getElementById("loan-amount").value = "10000";
+  document.getElementById("loan-years").value = "5";
+  document.getElementById("loan-rate").value = "5";
+
+  // Calculate and display the initial monthly payment
+  update();
+}
+
+// Function to update the UI with the calculated monthly payment
+function update() {
+  const values = getCurrentUIValues();
+  const monthlyPayment = calculateMonthlyPayment(values);
+  updateMonthly(monthlyPayment);
+}
+
+// Function to calculate the monthly payment based on input values
+function calculateMonthlyPayment(values) {
+  const { amount, years, rate } = values;
+  const monthlyRate = rate / 12 / 100; // Convert annual rate to monthly and percentage to decimal
+  const numberOfPayments = years * 12; // Convert years to months
+  const denominator = Math.pow(1 + monthlyRate, numberOfPayments) - 1;
+  const monthlyPayment = (amount * monthlyRate) / denominator;
+  return monthlyPayment.toFixed(2); // Round to two decimal places
+}
+
+// Function to update the UI with the calculated monthly payment
+function updateMonthly(monthly) {
+  const monthlyPaymentElement = document.getElementById("monthly-payment");
+  monthlyPaymentElement.textContent = `$${monthly}`;
+}
